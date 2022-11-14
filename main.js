078c19b3ec2370c214812efd699da455db4eb407
ashikcse045 <ashikcse321@gmail.com>
2022-11-14 14:33:26 +0600

    animated progress bar with every scroll animation

---
+++ main.js
@@ -0,0 +1,34 @@
+const progress_section = document.querySelector('.progress_bar_section');
+
+const progress_indicator = document.querySelectorAll('.indicator');
+
+function showProgress(){
+    progress_indicator.forEach(progressBar => {
+        const value = progressBar.dataset.progress;
+
+        progressBar.style.opacity = 1;
+        progressBar.style.width = `${value}%`;
+    })
+}
+
+function hideProgress()
+{
+    progress_indicator.forEach(p => {
+        p.style.opacity = 0;
+        p.style.width = 0;
+    })
+}
+
+window.addEventListener('scroll', () => {
+    const selec_pos = progress_section.getBoundingClientRect().top;
+
+    const screen_pos = window.innerHeight;
+
+    if(selec_pos < screen_pos)
+    {
+        showProgress();
+    }
+    else{
+        hideProgress();
+    }
+});
